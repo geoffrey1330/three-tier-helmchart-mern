59ff1ed7c9e325eb4c4c5998be3fa8f8991f9bc9
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-08-29 14:59:52 +0100

    modifed manifest file

--- backend/src/routes/todo-routes.js
+++ backend/src/routes/todo-routes.js
@@ -29,7 +29,7 @@ routes.post('/todo/add', auth, async (req, res) => {
     }
 })
 
-routes.post('/todo/list', auth, async (req, res) => {
+routes.get('/todo/list', auth, async (req, res) => {
     try {
 
         const todos = await ToDos.find({
